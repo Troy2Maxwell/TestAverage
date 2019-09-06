5bbef0611a92f324265177cc1170b6658269d6e2
Troy Maxwell <zephyrmaxwell@hotmail.com>
2019-09-06 08:50:04 -0500

    created project

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TestAverage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please work");
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
